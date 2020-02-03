fa3e79cffec5f277eaceb5e9836fe80f1c90b76a
Edoardo Balzani <eb162@ad.nyu.edu>
2020-02-03 09:24:03 -0500

    added a check on file size

--- firefly-monkey/Analysis/methods__read/FixEvents_nev.m
+++ firefly-monkey/Analysis/methods__read/FixEvents_nev.m
@@ -3,8 +3,10 @@ function events_nev = FixEvents_nev(events_nev,behv_trials)
 events_smr = cell2mat({behv_trials.events});
 ntrls_smr = length(events_smr);
 ntrls_nev = length(events_nev.t_beg);
+
 if (ntrls_smr - ntrls_nev) < 0
     warning('Mismatch in number of trials in NEV and SMR -- could not fix the problem!');
+
 else
     ntrls_missed = ntrls_smr - ntrls_nev;
     % check if neural recording was not started on time
@@ -18,5 +20,20 @@ else
         events_nev.t_beg = [nan(1,ntrls_missed) events_nev.t_beg];
         events_nev.t_end = [nan(1,ntrls_missed) events_nev.t_end];
     end
+    ntrls_end_nev = length(events_nev.t_end);
+    if (ntrls_smr == (ntrls_end_nev + 1)) 
+        % possibly recording interrupted before in the nev
+        iti_nev = diff(events_nev.t_beg);
+        iti_smr = diff([events_smr.t_beg]);
+        badindx = (iti_smr>10 | iti_nev>10 | isnan(iti_nev));
+        iti_smr = iti_smr(~badindx); iti_nev = iti_nev(~badindx);
+        if corr(iti_smr(:),iti_nev(:)) > 0.99
+            warning('Mismatch in number of trials in NEV and SMR -- problem most likely fixed!');
+            % yes, neural recording was not started on time
+            events_nev.t_end = [events_nev.t_end nan(1,1)];
+            events_nev.t_beg(end) = nan;
+        end
+    end
+ end
 end
 
