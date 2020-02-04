a4e52c4d4563fa20cf938bccc4bc0babf6f5505e
edoardo <edoardo.balzani87@gmail.com>
2020-02-04 09:55:27 -0500

    local changes overwritten by merge

--- firefly-monkey/Analysis/methods__helper/PrepareData.m
+++ firefly-monkey/Analysis/methods__helper/PrepareData.m
@@ -51,6 +51,39 @@ if prs.addconcat
 %     save([prs.sess_date,'.mat'],'behv_stats','data_concat','lfps','prs','trials_behv','units');
 else
     cd(prs.filepath_neur);
-    disp(['Saving exported data: ', exportname]);
-    save(exportname,'behv_stats','lfps','prs','trials_behv','units');
+    if prs.compute_spectrum
+        lfp_beta = struct();
+        lfp_theta = struct();
+        lfp_alpha = struct();
+        lfps_new = struct();
+        for ch = 1: length(lfps)
+            lfp_beta(ch).trials = struct();
+            lfp_theta(ch).trials = struct();
+            lfp_alpha(ch).trials = struct();
+            lfps_new(ch).trials = struct('lfp',[]);
+            lfps_new(ch).channel_id = lfps(ch).channel_id;
+            lfps_new(ch).electrode_id = lfps(ch).electrode_id;
+            lfps_new(ch).brain_area = lfps(ch).brain_area;
+            for tr = 1:length(lfps(ch).trials)
+                lfp_beta(ch).trials(tr).lfp_beta = lfps(ch).trials(tr).lfp_beta;
+                lfp_theta(ch).trials(tr).lfp_beta = lfps(ch).trials(tr).lfp_theta;
+                lfp_alpha(ch).trials(tr).lfp_alpha = lfps(ch).trials(tr).lfp_alpha;
+                lfps_new(ch).trials(tr) = rmfield(lfps(ch).trials(tr),{'lfp_beta','lfp_alpha','lfp_theta'});
+
+            end
+        end
+        
+        lfps = lfp_new;
+        disp(['Saving exported data: ', exportname]);
+        save(exportname,'behv_stats','lfps','prs','trials_behv','units');
+        exportname2 = ['lfp_beta_','m',num2str(monk_id),'s',num2str(sess_id),'.mat'];
+        save(exportname2,'lfps_beta');
+        exportname2 = ['lfp_alpha_','m',num2str(monk_id),'s',num2str(sess_id),'.mat'];
+        save(exportname2,'lfps_alpha');
+        exportname2 = ['lfp_theta_','m',num2str(monk_id),'s',num2str(sess_id),'.mat'];
+        save(exportname2,'lfps_theta');
+   else
+        disp(['Saving exported data: ', exportname]);
+        save(exportname,'behv_stats','lfps','prs','trials_behv','units');
+    end
 end
