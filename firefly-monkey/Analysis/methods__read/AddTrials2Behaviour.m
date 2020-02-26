bb44cd6fa9855e7a1fc8bf6a957914b1edb81060
edoardo <edoardo.balzani87@gmail.com>
2020-02-26 11:51:12 -0500

    BUG FIX not debugged yet

--- firefly-monkey/Analysis/methods__read/AddTrials2Behaviour.m
+++ firefly-monkey/Analysis/methods__read/AddTrials2Behaviour.m
@@ -4,9 +4,9 @@ trials = []; % initialise
 
 %% list all files to read
 flist_log=dir('*.log'); 
-for i=1:length(flist_log), fnum_log(i) = str2num(flist_log(i).name(5:end-4)); end
+for i=1:length(flist_log), fnum_log(i) = str2num(flist_log(i).name(end-6:end-4)); end
 flist_smr=dir('*.smr');
-for i=1:length(flist_smr), fnum_smr(i) = str2num(flist_smr(i).name(5:end-4)); end
+for i=1:length(flist_smr), fnum_smr(i) = str2num(flist_smr(i).name(end-6:end-4)); end
 % flist_mat=dir('*.mat');
 % for i=1:length(flist_mat), fnum_mat(i) = str2num(flist_mat(i).name(end-6:end-4)); end
 nfiles = length(flist_log);
@@ -21,23 +21,21 @@ for i=1:nfiles
     else indx_smr = find(fnum_smr >= fnum_log(i)); end
     trials_smr = [];
     for j = indx_smr
-        %         cedpath = getenv('CEDS64ML');
-        %         addpath( cedpath );
-        %         CEDS64LoadLib( cedpath );
-        %         data_smr = (flist_smr(j).name);
-        %         data_smr = ImportSMR2(flist_smr(j).name);
-        %         unloadlibrary ceds64int;
         data_smr = ImportSMR(flist_smr(j).name);
         trials_smr = [trials_smr AddSMRData(data_smr,prs)];
     end
     % merge contents of .log and .smr files
     ntrls_log = length(trials_log); ntrls_smr = length(trials_smr);
     if ntrls_smr <= ntrls_log
-        for j=1:length(trials_smr), trials_temp(j) = catstruct(trials_smr(j),trials_log(j)) ; end
+%         for j=1:length(trials_smr), trials_temp(j) = catstruct(trials_smr(j),trials_log(j)) ; end
+        for j=1:length(trials_smr), trials_temp(j) = concat_smr_log(trials_smr(j),trials_log(j)) ; end
     else  % apply a very dirty fix if spike2 was not "stopped" on time (can happen when replaying stimulus movie)
-        for j=1:ntrls_log, trials_temp(j) = catstruct(trials_smr(j),trials_log(j)) ; end
+%         for j=1:ntrls_log, trials_temp(j) = catstruct(trials_smr(j),trials_log(j)) ; end
+        for j=1:ntrls_log, trials_temp(j) = concat_smr_log(trials_smr(j),trials_log(j)) ; end
         dummy_trials_log = trials_log(1:ntrls_smr-ntrls_log);
-        for j=1:(ntrls_smr-ntrls_log); trials_temp(ntrls_log+j) = catstruct(trials_smr(ntrls_log+j),dummy_trials_log(j)); end
+%         for j=1:(ntrls_smr-ntrls_log); trials_temp(ntrls_log+j) = catstruct(trials_smr(ntrls_log+j),dummy_trials_log(j)); end
+        for j=1:(ntrls_smr-ntrls_log); trials_temp(ntrls_log+j) = concat_smr_log(trials_smr(ntrls_log+j),dummy_trials_log(j)); end
+
     end
     % add contents of .mat file
 %     trials_temp = AddMATData(flist_mat(i).name,trials_temp);
